e36feb551ddbe209441fd66ff30cecc19fcb19e1
l11040 <l1104033837@gmail.com>
2025-05-08 21:31:33 +0900

    [Update] 임시 제품 리스트 요청 구성

--- src/app/management/products/page.tsx
+++ src/app/management/products/page.tsx
@@ -1,9 +1,21 @@
 import { Link } from "react-router-dom";
+import { IGetProduct } from "../../../lib/types/product";
+import useSWR from "swr";
+import {
+  GET_PRODUCTS_BY_CATEGORY,
+  getProductsByCategory,
+} from "../../../lib/api/products";
+import ProductsTable from "../../../components/domain/table/ProductsTable";
 
 export default function ManagementProductsPage() {
+  const { data: products, isLoading: isProductsLoading } = useSWR<
+    IGetProduct[]
+  >([GET_PRODUCTS_BY_CATEGORY, "all"], () => getProductsByCategory("all"));
+
   return (
     <>
       <div>제품 관리</div>
+      {!isProductsLoading && products && <ProductsTable data={products} />}
       <Link to="/">홈으로</Link>
     </>
   );
