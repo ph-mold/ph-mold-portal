2c6bf39adbf01d0c56f64ae68be98d6f51650400
l11040 <l1104033837@gmail.com>
2025-05-13 21:15:01 +0900

    [Update] 태그 form 데이터 정리

--- src/components/management/products/TagEditor.tsx
+++ src/components/management/products/TagEditor.tsx
@@ -1,43 +1,71 @@
 import { Button } from "@ph-mold/ph-ui";
 import { PlusSquareIcon, X } from "lucide-react";
-import { FieldArrayWithId } from "react-hook-form";
-import { IGetProductInfo } from "../../../lib/types/product";
+import { UseFieldArrayReturn } from "react-hook-form";
+import { IGetProductInfo, ITag } from "../../../lib/types/product";
+import { useState } from "react";
+import AddTagModal from "./AddTagModal";
 
 interface Props {
-  tags: FieldArrayWithId<IGetProductInfo, "tags", "id">[];
-  remove: (index: number) => void;
-  openAddTagModal: () => void;
+  field: UseFieldArrayReturn<IGetProductInfo, "tags", "fieldId">;
 }
 
-export default function TagEditor({ tags, remove, openAddTagModal }: Props) {
+export default function TagEditor({ field }: Props) {
+  const [openAddTag, setOpenAddTag] = useState(false);
+
+  const handleRemove = (idx: number) => {
+    const current = field.fields[idx];
+    if (current.flag === "new") {
+      field.remove(idx);
+    } else {
+      field.update(idx, {
+        ...current,
+        flag: "delete",
+      });
+    }
+  };
+
   return (
-    <div className="border-signature border rounded-lg px-4 pt-2 pb-4">
-      <div className="flex flex-row justify-between items-center mb-2">
-        <p className="text-foreground2">태그</p>
-        <Button
-          variant="text"
-          size="small"
-          startIcon={<PlusSquareIcon />}
-          onClick={openAddTagModal}
-        >
-          추가
-        </Button>
-      </div>
-      <div className="gap-2 flex flex-row flex-wrap ">
-        {tags.map((tag, idx) => (
-          <p
-            key={tag.key}
-            className="group bg-background2 text-signature h-7 rounded-md px-2 py-1 text-sm text-nowrap flex items-center gap-2 hover:bg-error hover:text-white transition-colors"
+    <>
+      <AddTagModal
+        open={openAddTag}
+        setOpen={setOpenAddTag}
+        addTagAction={(tag: ITag) =>
+          field.append({
+            ...tag,
+            flag: "new",
+          })
+        }
+      />
+      <div className="border-signature border rounded-lg px-4 pt-2 pb-4">
+        <div className="flex flex-row justify-between items-center mb-2">
+          <p className="text-foreground2">태그</p>
+          <Button
+            variant="text"
+            size="small"
+            startIcon={<PlusSquareIcon />}
+            onClick={() => setOpenAddTag(true)}
           >
-            {tag.name}
-            <X
-              size={16}
-              onClick={() => remove(idx)}
-              className="cursor-pointer"
-            />
-          </p>
-        ))}
+            추가
+          </Button>
+        </div>
+        <div className="gap-2 flex flex-row flex-wrap">
+          {field.fields
+            .filter((tag) => tag.flag !== "delete")
+            .map((tag, idx) => (
+              <p
+                key={tag.key}
+                className="group bg-background2 text-signature h-7 rounded-md px-2 py-1 text-sm text-nowrap flex items-center gap-2 hover:bg-error hover:text-white transition-colors"
+              >
+                {tag.name}
+                <X
+                  size={16}
+                  onClick={() => handleRemove(idx)}
+                  className="cursor-pointer"
+                />
+              </p>
+            ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
