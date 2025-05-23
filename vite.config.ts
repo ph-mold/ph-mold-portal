c07bfe5de9ae769c449eafc121401591b06ffc7d
l11040 <l1104033837@gmail.com>
2025-05-23 13:45:28 +0900

    [Update] 환경 변수 분기기

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,30 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { createHtmlPlugin } from "vite-plugin-html";
 
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: "./",
-  build: {
-    outDir: "dist-react",
-  },
-  server: {
-    port: 5123,
-    strictPort: true,
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  return {
+    plugins: [
+      react(),
+      tailwindcss(),
+      createHtmlPlugin({
+        minify: true,
+        inject: {
+          data: {
+            apiUrl: env.VITE_API_BASE_URL,
+          },
+        },
+      }),
+    ],
+    base: "./",
+    build: {
+      outDir: "dist-react",
+    },
+    server: {
+      port: 5123,
+      strictPort: true,
+    },
+  };
 });
