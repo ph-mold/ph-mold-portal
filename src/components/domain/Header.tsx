a289d85777c92dd87fcc2f9a7030eda4c83b9f2c
l11040 <l1104033837@gmail.com>
2025-05-26 16:39:07 +0900

    [Chore]

--- src/components/domain/Header.tsx
+++ src/components/domain/Header.tsx
@@ -1,27 +1,16 @@
 import { Button } from "@ph-mold/ph-ui";
-import { ChevronLeft } from "lucide-react";
-import { Link, useNavigate } from "react-router-dom";
-import { useRecoilValue, useResetRecoilState, useSetRecoilState } from "recoil";
+import { ChevronLeft, CircleUserRound } from "lucide-react";
+import { Link } from "react-router-dom";
+import { useRecoilValue, useSetRecoilState } from "recoil";
 import { headerState } from "../../recoil/headerAtom";
 import { useUser } from "../../hooks/useUser";
 import { userState } from "../../recoil/authAtom";
-import { postLogout } from "../../lib/api/auth";
-import { clearToken } from "../../lib/electron/authPref";
 import { useEffect } from "react";
 
 export default function Header() {
   const header = useRecoilValue(headerState);
   const { user } = useUser();
   const setUser = useSetRecoilState(userState);
-  const navigate = useNavigate();
-  const resetUser = useResetRecoilState(userState);
-
-  const handleLogout = async () => {
-    await postLogout();
-    await clearToken();
-    resetUser();
-    navigate("/login");
-  };
 
   useEffect(() => {
     setUser(user ?? null);
@@ -38,17 +27,18 @@ export default function Header() {
         )}
         <h1 className="text-lg">{header.title}</h1>
         {header.leftSlot}
-        <div className="w-fit ml-auto h-full flex items-center gap-2 text-sm">
+        <div className="w-fit ml-auto h-full flex items-center gap-0.5 text-sm">
           {header.rightSlot}
           {user ? (
-            <>
-              <span>
-                {user.name} ({user.role})님
-              </span>
-              <Button size="small" variant="text" onClick={handleLogout}>
-                로그아웃
-              </Button>
-            </>
+            <Button
+              size="small"
+              variant="text"
+              color="secondary"
+              startIcon={<CircleUserRound />}
+            >
+              {user.name}
+              <span className="text-foreground2">님</span>
+            </Button>
           ) : (
             <Link to="/login">
               <Button size="small" variant="text">
