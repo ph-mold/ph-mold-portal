190ec6e0dfafbdad5f52af782491ff8b4d7bcf86
l11040 <l1104033837@gmail.com>
2025-06-16 16:54:01 +0900

    [Add] 라벨 스티커 이력 조회 및 PDF 재생성 기능 추가

--- src/lib/api/label-sticker.ts
+++ src/lib/api/label-sticker.ts
@@ -1,6 +1,6 @@
 import { axiosInstance } from "../axiosInstance";
 import { API } from "../constants/api";
-import { LabelSticker } from "../types/label-sticker";
+import { LabelSticker, LabelStickerListParams } from "../types/label-sticker";
 
 export const POST_LS_3510_PDF = "postLS3510PDF";
 export const postLS3510PDF = async (data: LabelSticker) => {
@@ -10,5 +10,30 @@ export const postLS3510PDF = async (data: LabelSticker) => {
       Accept: "application/pdf",
     },
   });
+  return response.data;
+};
+
+export const GET_LABEL_STICKER_HISTORIES = "getLabelStickerHistories";
+export const getLabelStickerHistories = async (
+  params: LabelStickerListParams
+) => {
+  const response = await axiosInstance.get(API.LABEL_STICKER.GET_HISTORIES, {
+    params,
+  });
+  return response.data;
+};
+
+export const POST_LS_3510_PDF_REGENERATE = "postLS3510PDFRegenerate";
+export const postLS3510PDFRegenerate = async (data: LabelSticker) => {
+  const response = await axiosInstance.post(
+    API.LABEL_STICKER.LS_3510_REGENERATE,
+    data,
+    {
+      responseType: "blob",
+      headers: {
+        Accept: "application/pdf",
+      },
+    }
+  );
   return response.data;
 };
