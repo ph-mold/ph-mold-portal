906bd5fb4e33099f9924c95840599ab831558e46
l11040 <l1104033837@gmail.com>
2025-06-20 14:52:17 +0900

    [Refactor] 라벨 스티커 페이지 컴포넌트 분리 및 코드 정리

--- src/components/label-sticker/index.ts
+++ src/components/label-sticker/index.ts
@@ -1,3 +1,4 @@
+export { LabelHistory } from "./LabelHistory";
 export { HistoryList } from "./HistoryList";
 export { HistoryListItem } from "./HistoryListItem";
 export { HistoryModal } from "./HistoryModal";
