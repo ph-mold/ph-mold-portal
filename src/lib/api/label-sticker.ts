2e1dbb52674a3500cc180b92d6746ce512cb52ec
l11040 <l1104033837@gmail.com>
2025-06-20 11:39:05 +0900

    [Add] 라벨 스티커 API에 LS-3509 및 LS-3509 재생성 추가

--- src/lib/api/label-sticker.ts
+++ src/lib/api/label-sticker.ts
@@ -13,16 +13,6 @@ export const postLS3510PDF = async (data: LabelSticker) => {
   return response.data;
 };
 
-export const GET_LABEL_STICKER_HISTORIES = "getLabelStickerHistories";
-export const getLabelStickerHistories = async (
-  params: LabelStickerListParams
-) => {
-  const response = await axiosInstance.get(API.LABEL_STICKER.GET_HISTORIES, {
-    params,
-  });
-  return response.data;
-};
-
 export const POST_LS_3510_PDF_REGENERATE = "postLS3510PDFRegenerate";
 export const postLS3510PDFRegenerate = async (
   data: LabelSticker,
@@ -39,5 +29,45 @@ export const postLS3510PDFRegenerate = async (
       signal,
     }
   );
+  return response.data;
+};
+
+export const POST_LS_3509_PDF = "postLS3509PDF";
+export const postLS3509PDF = async (data: LabelSticker) => {
+  const response = await axiosInstance.post(API.LABEL_STICKER.LS_3509, data, {
+    responseType: "blob",
+    headers: {
+      Accept: "application/pdf",
+    },
+  });
+  return response.data;
+};
+
+export const POST_LS_3509_PDF_REGENERATE = "postLS3509PDFRegenerate";
+export const postLS3509PDFRegenerate = async (
+  data: LabelSticker,
+  signal?: AbortSignal
+) => {
+  const response = await axiosInstance.post(
+    API.LABEL_STICKER.LS_3509_REGENERATE,
+    data,
+    {
+      responseType: "blob",
+      headers: {
+        Accept: "application/pdf",
+      },
+      signal,
+    }
+  );
+  return response.data;
+};
+
+export const GET_LABEL_STICKER_HISTORIES = "getLabelStickerHistories";
+export const getLabelStickerHistories = async (
+  params: LabelStickerListParams
+) => {
+  const response = await axiosInstance.get(API.LABEL_STICKER.GET_HISTORIES, {
+    params,
+  });
   return response.data;
 };
