5d2f0ba787cd53d067177f155cbbb3063dac476f
l11040 <l1104033837@gmail.com>
2025-06-30 17:10:43 +0900

    [Refactor] 로그인 폼을 Formik 및 Yup을 사용하도록 리팩토링

--- src/components/login/LoginForm.tsx
+++ src/components/login/LoginForm.tsx
@@ -1,6 +1,5 @@
 import { Button, Input } from "@ph-mold/ph-ui";
 import { useEffect, useState } from "react";
-import { useForm } from "react-hook-form";
 import {
   clearLoginEmail,
   getSavedLoginEmail,
@@ -14,6 +13,15 @@ import { useLocation, useNavigate } from "react-router-dom";
 import { useSetRecoilState } from "recoil";
 import { userState } from "../../recoil/authAtom";
 import { mutate } from "swr";
+import { Formik, FormikHelpers } from "formik";
+import * as Yup from "yup";
+
+const validationSchema = Yup.object().shape({
+  email: Yup.string()
+    .email("이메일 형식이 올바르지 않습니다.")
+    .required("이메일을 입력해주세요."),
+  password: Yup.string().required("비밀번호를 입력해주세요."),
+});
 
 export default function LoginForm() {
   const navigate = useNavigate();
@@ -21,47 +29,57 @@ export default function LoginForm() {
   const from = (location.state as { from?: Location })?.from?.pathname ?? "/";
   const setUser = useSetRecoilState(userState);
 
-  const { register, handleSubmit, setValue } = useForm<ILoginBody>({
-    defaultValues: {
-      email: "",
-      password: "",
-    },
-  });
   const [isSaveEmail, setIsSaveEmail] = useState(false);
+  const [initFormValues, setInitFormValues] = useState<ILoginBody>({
+    email: "",
+    password: "",
+  });
 
   useEffect(() => {
     (async () => {
-      const id = await getSavedLoginEmail();
-      if (id) {
-        setValue("email", id);
+      const savedEmail = await getSavedLoginEmail();
+      if (savedEmail) {
+        setInitFormValues({
+          email: savedEmail,
+          password: "",
+        });
         setIsSaveEmail(true);
       }
     })();
   }, []);
 
-  const onSubmit = async (data: ILoginBody) => {
-    const { accessToken, refreshToken, user } = await postLogin(data);
+  const onSubmit = async (
+    values: ILoginBody,
+    { setSubmitting }: FormikHelpers<ILoginBody>
+  ) => {
+    try {
+      setSubmitting(true);
+      const { accessToken, refreshToken, user } = await postLogin(values);
 
-    if (accessToken) {
-      await saveAccessToken(accessToken);
+      if (accessToken) {
+        await saveAccessToken(accessToken);
+      }
+      if (isElectron && refreshToken) {
+        await saveRefreshToken(refreshToken);
+      }
+
+      if (isSaveEmail) {
+        await saveLoginEmail(values.email);
+      } else {
+        await clearLoginEmail();
+      }
+
+      await mutate(GET_ME, user, false);
+      setUser(user);
+      navigate(from, { replace: true });
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setSubmitting(false);
     }
-    if (isElectron && refreshToken) {
-      await saveRefreshToken(refreshToken);
-    }
-
-    if (isSaveEmail) {
-      await saveLoginEmail(data.email);
-    } else {
-      await clearLoginEmail();
-    }
-
-    await mutate(GET_ME, user, false);
-    setUser(user);
-
-    navigate(from, { replace: true });
   };
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleIsSaveEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setIsSaveEmail(e.target.checked);
   };
 
@@ -69,25 +87,75 @@ export default function LoginForm() {
     <div className="basis-1/2 max-w-[400px] w-full bg-white/70 backdrop-blur-md flex min-h-svh items-center justify-center">
       <div className="flex flex-col gap-4 w-full px-8">
         <p className="text-2xl font-semibold text-center">로그인</p>
-        <form className="space-y-2" onSubmit={handleSubmit(onSubmit)}>
-          <Input label="이메일" {...register("email", { required: true })} />
-          <Input
-            label="비밀번호"
-            type="password"
-            {...register("password", { required: true })}
-          />
-          <label className="flex gap-1 cursor-pointer items-center ml-auto w-fit select-none">
-            <input
-              type="checkbox"
-              checked={isSaveEmail}
-              onChange={handleChange}
-            />
-            <span className="text-xs">이메일 저장</span>
-          </label>
-          <Button type="submit" fullWidth>
-            로그인
-          </Button>
-        </form>
+        <Formik<ILoginBody>
+          initialValues={initFormValues}
+          onSubmit={onSubmit}
+          validationSchema={validationSchema}
+          validateOnChange={true}
+          validateOnBlur={true}
+          enableReinitialize={true}
+        >
+          {({
+            values,
+            handleChange,
+            handleBlur,
+            isSubmitting,
+            setTouched,
+            submitForm,
+            errors,
+            touched,
+          }) => (
+            <form className="space-y-2">
+              <Input
+                label="이메일"
+                name="email"
+                value={values.email}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                error={!!(errors.email && touched.email)}
+                helperText={
+                  errors.email && touched.email ? errors.email : undefined
+                }
+              />
+              <Input
+                label="비밀번호"
+                type="password"
+                name="password"
+                value={values.password}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                error={!!(errors.password && touched.password)}
+                helperText={
+                  errors.password && touched.password
+                    ? errors.password
+                    : undefined
+                }
+              />
+              <label className="flex gap-1 cursor-pointer items-center ml-auto w-fit select-none">
+                <input
+                  type="checkbox"
+                  checked={isSaveEmail}
+                  onChange={handleIsSaveEmailChange}
+                />
+                <span className="text-xs">이메일 저장</span>
+              </label>
+              <Button
+                type="button"
+                fullWidth
+                loading={isSubmitting}
+                onClick={() => {
+                  setTouched({
+                    email: true,
+                    password: true,
+                  });
+                  submitForm();
+                }}
+              >
+                로그인
+              </Button>
+            </form>
+          )}
+        </Formik>
         <Button variant="text" className="text-xs !w-fit mx-auto">
           비밀번호를 잊어버리셨나요?
         </Button>
