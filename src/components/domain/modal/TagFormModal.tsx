92ef7cbe817dcd6b40c6a8f0224ab862bcc13c41
l11040 <l1104033837@gmail.com>
2025-07-01 13:55:45 +0900

    [Refactor] TagFormModal 컴포넌트를 Formik 및 Yup을 사용하여 리팩토링하고, 폼 유효성 검사를 추가

--- src/components/domain/modal/TagFormModal.tsx
+++ src/components/domain/modal/TagFormModal.tsx
@@ -1,7 +1,13 @@
 import { Modal, Input, Button } from "@ph-mold/ph-ui";
-import { ITag } from "../../../lib/types/tag";
+import { CreateTagDto, ITag } from "../../../lib/types/tag";
 import { useState, useEffect } from "react";
+import { Form, Formik, FormikHelpers } from "formik";
+import * as yup from "yup";
 
+const validationSchema = yup.object({
+  name: yup.string().required("이름을 입력해주세요"),
+  key: yup.string().required("키를 입력해주세요"),
+});
 interface Props {
   open: boolean;
   onClose: () => void;
@@ -15,19 +21,30 @@ export default function TagFormModal({
   onSubmit,
   editingTag,
 }: Props) {
-  const [formData, setFormData] = useState({ name: "", key: "" });
+  const [initFormValues, setInitFormValues] = useState({ name: "", key: "" });
 
   useEffect(() => {
     if (editingTag) {
-      setFormData({ name: editingTag.name, key: editingTag.key });
+      setInitFormValues({ name: editingTag.name, key: editingTag.key });
     } else {
-      setFormData({ name: "", key: "" });
+      setInitFormValues({ name: "", key: "" });
     }
   }, [editingTag]);
 
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-    onSubmit(formData);
+  const handleSubmit = (
+    values: CreateTagDto,
+    { setSubmitting, resetForm }: FormikHelpers<CreateTagDto>
+  ) => {
+    try {
+      setSubmitting(true);
+      onSubmit(values);
+      resetForm();
+      onClose();
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setSubmitting(false);
+    }
   };
 
   return (
@@ -37,38 +54,81 @@ export default function TagFormModal({
       onClose={onClose}
       bodyClassName="!p-4"
     >
-      <form onSubmit={handleSubmit} className="space-y-4">
-        <div>
-          <label className="block text-sm font-medium mb-1">이름</label>
-          <Input
-            value={formData.name}
-            onChange={(e) =>
-              setFormData((prev) => ({ ...prev, name: e.target.value }))
-            }
-            required
-            placeholder="이름을 입력해주세요"
-          />
-        </div>
+      <Formik<CreateTagDto>
+        initialValues={initFormValues}
+        validationSchema={validationSchema}
+        onSubmit={handleSubmit}
+        validateOnBlur={true}
+        validateOnChange={true}
+        enableReinitialize={true}
+      >
+        {({
+          values,
+          handleChange,
+          handleBlur,
+          errors,
+          touched,
+          isSubmitting,
+          submitForm,
+          setTouched,
+          resetForm,
+        }) => (
+          <Form className="space-y-4">
+            <div>
+              <label className="block text-sm font-medium mb-1">이름</label>
+              <Input
+                required
+                name="name"
+                placeholder="이름을 입력해주세요"
+                value={values.name}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                error={!!(errors.name && touched.name)}
+                helperText={
+                  errors.name && touched.name ? errors.name : undefined
+                }
+              />
+            </div>
 
-        <div>
-          <label className="block text-sm font-medium mb-1">키</label>
-          <Input
-            value={formData.key}
-            onChange={(e) =>
-              setFormData((prev) => ({ ...prev, key: e.target.value }))
-            }
-            required
-            placeholder="키를 입력해주세요"
-          />
-        </div>
+            <div>
+              <label className="block text-sm font-medium mb-1">키</label>
+              <Input
+                required
+                name="key"
+                placeholder="키를 입력해주세요"
+                value={values.key}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                error={!!(errors.key && touched.key)}
+                helperText={errors.key && touched.key ? errors.key : undefined}
+              />
+            </div>
 
-        <div className="flex justify-end gap-2">
-          <Button variant="text" onClick={onClose}>
-            취소
-          </Button>
-          <Button type="submit">{editingTag ? "수정" : "추가"}</Button>
-        </div>
-      </form>
+            <div className="flex justify-end gap-2">
+              <Button
+                variant="text"
+                onClick={() => {
+                  resetForm();
+                  onClose();
+                }}
+                disabled={isSubmitting}
+              >
+                취소
+              </Button>
+              <Button
+                type="button"
+                loading={isSubmitting}
+                onClick={() => {
+                  setTouched({ name: true, key: true });
+                  submitForm();
+                }}
+              >
+                {editingTag ? "수정" : "추가"}
+              </Button>
+            </div>
+          </Form>
+        )}
+      </Formik>
     </Modal>
   );
 }
