e1d54d5695fa14d9e0205f89de29e6015f46dccf
l11040 <l1104033837@gmail.com>
2025-07-18 17:00:36 +0900

    [Update] 모바일 환경에서 Header 컴포넌트 표시 조건 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,21 @@
 import { Outlet } from "react-router-dom";
 import Header from "@/components/common/layout/Header";
 import RequireAuth from "@/components/common/RequireAuth";
+import clsx from "clsx";
 
 export default function RootLayout() {
+  const isRN = navigator.userAgent.includes("app");
   return (
     <RequireAuth>
       <div className="flex flex-col h-screen w-screen overflow-hidden">
-        <Header />
-        <main className="mt-16 flex-1 overflow-hidden">
+        {isRN ? null : <Header />}
+        <main
+          className={clsx(
+            "flex-1 overflow-hidden",
+            isRN && "mt-0",
+            !isRN && "mt-16"
+          )}
+        >
           <Outlet />
         </main>
         <div id="modal-root" />
