1334412dac837bccfc934b435544964f51fa3078
l11040 <l1104033837@gmail.com>
2025-08-20 12:20:15 +0900

    [Chore]

--- src/hooks/usePagination.ts
+++ src/hooks/usePagination.ts
@@ -1,5 +1,10 @@
 import { useSearchParams } from "react-router-dom";
 
+//TODO 추후 data를 받아 페이지네이션 계산하도록 수정
+/*
+const currentItems = data?.items ?? [];
+  const totalPages = Math.ceil((data?.total || 0) / ITEMS_PER_PAGE);
+*/
 export function usePagination() {
   const [searchParams, setSearchParams] = useSearchParams();
   const currentPage = Number(searchParams.get("page")) || 1;
